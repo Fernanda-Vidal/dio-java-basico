88f1e143f425248b611040ac841eb76eb24d82cf
Fernanda-Vidal <fevidal.dev@gmail.com>
2023-08-27 08:03:37 -0300

    feat: add operadores

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        System.out.println(numero);
+
+        // VAI PRINTAR E DEPOIS INCREMENTAR
+        System.out.println(numero ++);
+        
+        System.out.println(numero);
+
+        // VAI INCREMENTAR E DEPOIS PRINTAR
+        System.out.println(++ numero);
+
+    }
+}

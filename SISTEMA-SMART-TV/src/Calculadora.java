1251467a08f05011bcf09fa1046582a68e8e2da5
Fernanda-Vidal <fevidal.dev@gmail.com>
2023-08-27 09:48:01 -0300

    feat: add Java Doc

---
+++ SISTEMA-SMART-TV/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ <h1>Calculadora</h1>
+ <p>A Calculadora realiza operações matemáticas entre números inteiros</p>
+ <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos
+ recursos oferecidos pelo autor.
+ 
+ @author  Fernanda Vidal
+ @version 1.0
+ @since 01/01/2023
+ */
+public class Calculadora {
+    /**
+     *Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parêmetro do método
+     * @return int o resultado deste método é a soma dos dois números 
+     */
+
+     public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+     }
+}
+ 
